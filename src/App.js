500f73a4926e7096f9b72bf5353e58e24f5b6ce4
Francisca Moore <fraanmoore@gmail.com>
2024-02-13 21:21:20 -0300

    agregar bootstrap y inicio de navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <Navbar/>
       </header>
     </div>
   );
