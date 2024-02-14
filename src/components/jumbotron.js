53087a789452b74a8475bfc8813bc98e8895d162
Francisca Moore <fraanmoore@gmail.com>
2024-02-13 22:00:46 -0300

    change jumbotron

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -2,13 +2,24 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-      <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Example button</button>
-      </div>
-    </div>
+        <section class="min-vh-70 o-hidden d-flex flex-column justify-content-center">
+            <div class="container">
+                <div class="row justify-content-center text-center align-items-center">
+                    <div class="col-xl-8 col-lg-9 col-md-10 layer-3 aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
+                        <h1 class="display-3">
+                            Launch on time,<br />look on-trend.
+                        </h1>
+                        <div class="mb-4">
+                            <p class="lead px-xl-5">
+                                Save countless hours of design and development and ship performant sites with killer looks.
+                            </p>
+                        </div>
+                        <a href="#demos" class="btn btn-lg btn-white mx-1" data-smooth-scroll="">View Demos</a>
+                        <a href="https://themes.getbootstrap.com/product/leap-multipurpose-bootstrap-theme/" class="btn btn-lg btn-primary-3 mx-1">Purchase</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
