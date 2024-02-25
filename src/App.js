7b017bea8bfe9a842c7a12bb542742b90dac764f
Francisca Moore <fraanmoore@gmail.com>
2024-02-25 17:55:24 -0300

    change front page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <Navbar/>
-       <Jumbotron/>
-       <BodyCard/>
-       <Footer/>
+  
       </header>
     </div>
   );
