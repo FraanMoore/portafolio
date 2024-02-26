b8486799d59e99d64d46bfa36562d3631f31935f
Francisca Moore <fraanmoore@gmail.com>
2024-02-25 21:10:17 -0300

    front page with routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar';
+import AboutMe from './components/aboutme';
+import Projects from './components/projects';
+import Contact from './components/contact';
 
 function App() {
   return (
-
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/AboutMe" element={<AboutMe />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
